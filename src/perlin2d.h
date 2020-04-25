9d4fcaae1917c70a8239830b649d8762a6dd8dfe
jgesc <37676259+jgesc@users.noreply.github.com>
2020-04-25 20:25:19 +0200

    Fix DOT macro

--- src/perlin2d.h
+++ src/perlin2d.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define NORM(X,Y) (X + Y) ? X / (X + Y) : 0
-#define DOT(A,B) A[0] * B[0] + A[1] * B[1]
+#define DOT(A,B) (A)[0] * (B)[0] + (A)[1] * (B)[1]
 #define LERP(A,B,T) (A * T + B * (1 - T))
 
 int perlin(float * grid, size_t height, size_t width, unsigned int t, float a, unsigned int seed);
