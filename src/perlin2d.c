9d6891378871aa750e9b77172d0e106f1cfc8681
jgesc <37676259+jgesc@users.noreply.github.com>
2020-04-26 20:33:53 +0200

    Fix many bugs. TODO: fix seams

--- src/perlin2d.c
+++ src/perlin2d.c
@@ -9,14 +9,17 @@ int perlin2d(float * grid, size_t height, size_t width, unsigned int t, float a,
   srand(seed);
 
   // Allocate space for random vector grid
-  size_t vgridxl = width % t ? width / t + 1 : width / t;
-  size_t vgridyl = height % t ? height / t + 1 : height / t;
+  size_t vgridxl = width % t ? width / t + 2 : width / t + 1;
+  size_t vgridyl = height % t ? height / t + 2 : height / t + 1;
   float * vgrid = malloc(vgridxl * vgridyl * 2 * sizeof(float));
   if(!vgrid) return 0;
   // Fill random vector grid
   for(i = 0; i < vgridxl * vgridyl * 2; i += 2)
   {
     // Generate random unitary 2D vector
+    // float x = (float)rand() / RAND_MAX * 2 - 1;
+    // float y = (float)rand() / RAND_MAX * 2 - 1;
+
     float x = (float)rand() / RAND_MAX;
     float y = (float)rand() / RAND_MAX;
     float xn = NORM(x, y);
@@ -28,39 +31,61 @@ int perlin2d(float * grid, size_t height, size_t width, unsigned int t, float a,
   }
 
   // Interpolate values
-  for(i = 0; i < width; i++)
+  for(i = 0; i < height; i++)
   {
-    for(j = 0; j < height; j++)
+    for(j = 0; j < width; j++)
     {
       // Get top-left random vector index
-      int x = i / t;
-      int y = i % t;
+      int x = j / t;
+      int y = i / t;
       // Get relative position in cell
-      float u = i % t;
-      float v = j % t;
+      float u = j % t + 1;
+      float v = i % t + 1;
 
       // Calculate corner-point vectors (clockwise order)
-      float vec1[2] = {NORM(u, v), NORM(v, u)};
-      float vec2[2] = {NORM(t - u, v), NORM(v, t - u)};
-      float vec3[2] = {NORM(t - u, t - v), NORM(t - v, t - u)};
-      float vec4[2] = {NORM(y, t - v), NORM(t - v, y)};
-
-      printf("%f\n", vec2[0]);
+      float vec1[2] = {u / t, v / t};
+      float vec2[2] = {-(t - u) / t, v / t};
+      float vec3[2] = {-(t - u) / t, -(t - v) / t};
+      float vec4[2] = {u / t, -(t - v) / t};
 
       // Calculate dot products
       float dots[4] =
       {
-        DOT(vec1, vgrid + (y * vgridxl + x * 2)),
-        DOT(vec2, vgrid + (y * vgridxl + (x + 1) * 2)),
-        DOT(vec3, vgrid + ((y + 1) * vgridxl + (x + 1) * 2)),
-        DOT(vec4, vgrid + ((y + 1) * vgridxl + x * 2))
+        DOT(vec1, vgrid + ((y * vgridxl + x) * 2)),
+        DOT(vec2, vgrid + ((y * vgridxl + (x + 1)) * 2)),
+        DOT(vec3, vgrid + (((y + 1) * vgridxl + (x + 1)) * 2)),
+        DOT(vec4, vgrid + (((y + 1) * vgridxl + x) * 2))
       };
 
       // Interpolate
-      float a = LERP(dots[0], dots[1], u / t);
-      float b = LERP(dots[4], dots[3], u / t);
-      float val = LERP(a, b, v / t);
+      float avgt = LERP(dots[0], dots[1], (u - 1) / (t - 1));
+      float avgb = LERP(dots[3], dots[2], (u - 1) / (t - 1));
+      float val = LERP(avgt, avgb, (v - 1) / (t - 1));
 
+    //   float val;
+    //   if(u <= t / 2)
+    //   {
+    //     if(v <= t / 2)
+    //     {
+    //       val = dots[0];
+    //     }
+    //     else
+    //     {
+    //       val = dots[3];
+    //     }
+    //   }
+    //   else
+    //   {
+    //     if(v <= t / 2)
+    //     {
+    //       val = dots[1];
+    //     }
+    //     else
+    //     {
+    //       val = dots[2];
+    //     }
+    //   }
+    //
       // Store result
       grid[i * width + j] = val * a;
     }
--- src/perlin2d.h
+++ src/perlin2d.h
@@ -2,10 +2,11 @@
 #define __PERLIN2D_H__
 
 #include <stdlib.h>
+#include <math.h>
 
-#define NORM(X,Y) ((X) + (Y)) != 0 ? (X) / ((X) + (Y)) : 0
-#define DOT(A,B) (A)[0] * (B)[0] + (A)[1] * (B)[1]
-#define LERP(A,B,T) (A * T + B * (1 - T))
+#define NORM(X,Y) ((sqrt((X)*(X) + (Y)*(Y))) != 0 ? (X) / (sqrt((X)*(X) + (Y)*(Y))) : 0)
+#define DOT(A,B) ((A)[0] * (B)[0] + (A)[1] * (B)[1])
+#define LERP(A,B,T) (A * (1 - T) + B * T)
 
 int perlin2d(float * grid, size_t height, size_t width, unsigned int t, float a, unsigned int seed);
 
